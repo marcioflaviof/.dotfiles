c0eadaa7449a706c05052c9aadf498a10b34fb92
Marcio Flavio <mflaviof1995@gmail.com>
2022-06-26 14:35:10 -0300

    Updating nvimtree configs and fixing override

--- .config/NvChad/custom/override.lua
+++ .config/NvChad/custom/override.lua
@@ -1,35 +1,82 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "html",
-    "css",
-    "javascript",
-    "json",
-    "markdown",
-    "bash",
-    "lua",
-    "ruby",
-    "tsx",
-    "typescript",
-  },
+	ensure_installed = {
+		"vim",
+		"html",
+		"css",
+		"javascript",
+		"json",
+		"markdown",
+		"bash",
+		"lua",
+		"ruby",
+		"tsx",
+		"typescript",
+	},
 }
 
 M.nvimtree = {
-  git = {
-    enable = true,
-    ignore = false,
-  },
+	git = {
+		enable = true,
+		ignore = false,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+
+	view = {
+		width = 30,
+		height = 30,
+		hide_root_folder = false,
+		auto_resize = true,
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = { "l", "<CR>", "o" }, action = "edit" },
+				{ key = "h", action = "close_node" },
+				{ key = "v", action = "vsplit" },
+			},
+		},
+		number = false,
+		relativenumber = false,
+	},
+}
+
+M.cmp = {
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
+		{ name = "neorg" },
+	},
+	experimental = {
+		ghost_text = true,
+	},
+}
+
+M.blankline = {
+	filetype_exclude = {
+		"help",
+		"terminal",
+		"alpha",
+		"packer",
+		"lspinfo",
+		"TelescopePrompt",
+		"TelescopeResults",
+		"nvchad_cheatsheet",
+		"lsp-installer",
+		"norg",
+		"",
+	},
 }
 
 return M
