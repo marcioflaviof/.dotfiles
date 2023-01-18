82f25a630c90b7a47361bcafd265d21efaaa54aa
Marcio Flavio <mflaviof1995@gmail.com>
2023-01-18 08:45:40 -0300

    fix(git-blame): Fix bug in git blame and also update plugins

---
+++ .config/fish/completions/asdf.fish
@@ -0,0 +1,1 @@
+/home/mf/.asdf/completions/asdf.fish
--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -55,6 +55,16 @@ return lazy.setup({
 		dependencies = "hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 	},
+	{
+		"roobert/tailwindcss-colorizer-cmp.nvim",
+		-- optionally, override the default options:
+		config = function()
+			require("tailwindcss-colorizer-cmp").setup({
+				color_square_width = 2,
+			})
+		end,
+	},
+
 	-- LSP
 	"neovim/nvim-lspconfig",
 
