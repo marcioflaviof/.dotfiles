a119f58004b8b60f07a5a91ca3a221216511002f
Marcio Flavio <mflaviof1995@gmail.com>
2025-01-19 14:01:41 -0300

    feat: change telescope to snacks

--- .config/nvim/lua/plugins/qol.lua
+++ .config/nvim/lua/plugins/qol.lua
@@ -1,3 +1,10 @@
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 return {
   {
     "folke/snacks.nvim",
@@ -10,6 +17,13 @@ return {
       indent = { enabled = true, animate = { enabled = false } },
       dashboard = { enabled = false },
       statuscolumn = { enabled = false },
+      picker = {
+        enabled = true,
+        layout = {
+          preview = "main",
+          preset = "ivy",
+        },
+      },
       notifier = {
         enabled = false,
         timeout = 3000,
@@ -28,9 +42,21 @@ return {
     keys = {
       { "<leader>bd", function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
       { "<leader>gl", function() Snacks.git.blame_line() end,          desc = "Git Blame Line" },
-      { "<c-\\>",     function() Snacks.terminal() end,                desc = "Toggle Terminal", mode = { "n", "i", "t" } },
-      { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",  mode = { "n", "t" } },
-      { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",  mode = { "n", "t" } },
+      { "<c-\\>",     function() Snacks.terminal() end,                desc = "Toggle Terminal",          mode = { "n", "i", "t" } },
+      { "]]",         function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",           mode = { "n", "t" } },
+      { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",           mode = { "n", "t" } },
+
+      -- picker
+      { "<leader>f",  function() Snacks.picker.files() end,            desc = "Find Files" },
+      { "<leader>sh", function() Snacks.picker.help() end,             desc = "Help Pages" },
+      { "<leader>sg", function() Snacks.picker.grep() end,             desc = "Grep" },
+      { "<leader>sw", function() Snacks.picker.grep_word() end,        desc = "Visual selection or word", mode = { "n", "x" } },
+      { "<leader>sH", function() Snacks.picker.command_history() end,  desc = "Command History" },
+      { "<leader>sq", function() Snacks.picker.qflist() end,           desc = "Quickfix List" },
+      { "<leader>so", function() Snacks.picker.recent() end,           desc = "Recent" },
+      { "<leader>ss", function() Snacks.picker.lsp_symbols() end,      desc = "LSP Symbols" },
+      { "<leader>sk", function() Snacks.picker.keymaps() end,          desc = "Keymaps" },
+
     },
   }
 }
